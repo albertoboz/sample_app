fc11cbe49254e50ff4a73a05d2826abcb79b6de8
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-02-17 13:12:23 +0100

    5.3 fix static_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,8 +6,6 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(4)
   end
 
-  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
-
   def thank_you
     @name = params[:name]
     @email = params[:email]
