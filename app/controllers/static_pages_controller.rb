e1118f4df3e15ed30323808443e961492aa06377
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-02-17 11:44:28 +0100

    5.3 install Sendgrid plugin to send emails form Heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,16 @@ class StaticPagesController < ApplicationController
   def landing_page
     @products = Product.limit(4)
   end
+
+  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+        :to => 'alberto.chiorboli@gmail.com',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver_now
+  end
 end
