e16d954bba28e3f1e51a01bfe763386db691be2d
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-02-22 11:52:10 +0100

    5.4 create user scaffolding, install authentication gem Devise, add style to registering, logging in, logging out views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :users
   get 'user_mailer/contact_form'
 
   get 'static_pages/thank_you'
