53ed507030f32c4668932c591ed02bba3ae6b52c
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-02-28 11:56:21 +0100

    5.7 create comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
-
+  has_many :comments
   # search for a word similar to user input
   # check if production or development env.
   def self.search(search_term)
@@ -10,4 +10,13 @@ class Product < ApplicationRecord
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+  
 end
