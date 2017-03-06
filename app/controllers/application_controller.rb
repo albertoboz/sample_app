e17b7d36b2663875d7da1f6319de1ebb563dd591
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-03-06 14:10:29 +0100

    add first and last name fields to sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # redirected to the homepage and flash error appears
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password)
+    end
   end
 end
