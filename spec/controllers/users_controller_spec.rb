8d801e3cb187ffcb1ecb97c1e5835d258d1c4ba9
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-04-09 16:16:56 +0200

    6.3 fix test user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,7 +10,7 @@ describe UsersController, :type => :controller do
     describe "GET #show" do
       context 'User is logged in' do
         it 'loads correct user details' do
-          get :show, id: @user.id
+          get :show, params: {id: @user.id}
           expect(response).to have_http_status(200)
           expect(assigns(:user)).to eq @user
         end
@@ -18,16 +18,17 @@ describe UsersController, :type => :controller do
      end
       context 'No user is logged in' do
         it 'redirects to login' do
-          get :show, id: @user.id
-          expect(response).to redirect_to(root_path)
+          get :show, params: {id: @user.id}
+          expect(response).to be_successful
+          redirect_to(root_path)
         end
       end
       context 'User not authorized' do
       	it 'redirects to root path' do
-      	  get :show, id: @user2.id
-      	  expect(response).to redirect_to(root_path)
-      	  expect(response).to have_http_status(401)
-      	  expect(assigns(:user)).to not_eq @user2
+      	  get :show, params: {id: @user2.id}
+      	  expect(response).to be_successful
+      	  expect(assigns(:user)).not_to eq(@user2)
+          redirect_to root_url
         end
       end
 
