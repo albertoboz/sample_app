70e7498ce1a366f08d3cc9a33743442566e4fae2
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-04-11 14:42:54 +0200

    6.6 set up payments controller and create orders

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,7 +12,7 @@ class PaymentsController < ApplicationController
         :description => params[:stripeEmail]
       )
       if charge.paid
-        Order.create()
+        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
       end
     rescue Stripe::CardError => e
       # The card has been declined
