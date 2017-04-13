8794ee5b32cfc65c97aab55f547c69477fd5ae42
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-04-13 12:05:01 +0200

    6.9 add plugin elevateZoom

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -13,4 +13,14 @@ $(document).on('turbolinks:load', function(){
         return $(this).attr('data-score');
       }
     });
+
+  // elevate zoom
+  $('.img-zoom').elevateZoom({
+    responsive: true,
+    zoomWindowWidth:500,
+    zoomWindowHeight:300,
+    zoomWindowFadeIn: 500,
+    zoomWindowFadeOut: 750,
+    scrollZoom : true
+  });
 });
