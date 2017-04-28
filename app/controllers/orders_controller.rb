5357dd02ac1e9967b8e2c4a86a21b7c5baf58c13
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-04-28 16:31:46 +0200

    update orders view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @orders = Order.where("user_id =?", current_user.id).paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
